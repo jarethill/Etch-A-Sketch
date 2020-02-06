e0f07f9ea645ac9d988703169786231a3560ab40
Jaret Hill <jaret.hill@tuta.io>
2020-02-06 17:29:29 -0600

    Added realistic mode

--- scripts/main.js
+++ scripts/main.js
@@ -30,6 +30,79 @@ function deleteGrid() {
     }
 }
 
+// Apply event listeners to grid
+function applyEventListenersToGrid() {
+    const allDivs = document.querySelectorAll('.grid-item');
+    allDivs.forEach(div => {
+        let opacity = 1;
+
+        div.addEventListener('mouseover', e => {
+            if (mouseDown) {
+                if (rainbowOn) {
+                    // Generate 3 random numbers to create an RGB color, converts to hex & applies style to div
+                    let rgb = [];
+                    for (let i = 1; i <= 3; i++) {
+                        rgb.push(randomNum(0, 256));
+                    }
+
+                    const hexValue = rgbToHex(rgb[0], rgb[1], rgb[2]);
+
+                    div.setAttribute('style', `background-color: ${hexValue}; opacity: ${opacity}`);
+                    if (opacity > 0.4 && realisticMode === true) {
+                        opacity -= 0.1;
+                    }
+                } else {
+                    div.setAttribute('style', `background-color: #${colorPicker.value}; opacity: ${opacity}`);
+                    if (opacity > 0.4 && realisticMode === true) {
+                        opacity -= 0.1;
+                    }
+                }
+            } else if (rightMouseDown) {
+                div.removeAttribute('style');
+                opacity = 1;
+            }
+        });
+
+        div.addEventListener('mousedown', e => {
+            if (e.button === 0) {
+                if (rainbowOn) {
+                    // Generate 3 random numbers to create an RGB color, converts to hex & applies style to div
+                    let rgb = [];
+                    for (let i = 1; i <= 3; i++) {
+                        rgb.push(randomNum(0, 256));
+                    }
+
+                    const hexValue = rgbToHex(rgb[0], rgb[1], rgb[2]);
+
+                    div.setAttribute('style', `background-color: ${hexValue}; opacity: ${opacity}`);
+                    if (opacity > 0.4 && realisticMode === true) {
+                        opacity -= 0.1;
+                    }
+                } else {
+                    div.setAttribute('style', `background-color: #${colorPicker.value}; opacity: ${opacity}`);
+                    if (opacity > 0.4 && realisticMode === true) {
+                        opacity -= 0.1;
+                    }
+                }
+            } else if (e.button === 1) {
+                const styleAttribute = div.getAttribute('style');
+
+                if (styleAttribute !== null) {
+                    const colorPickerButton = document.querySelector('#color-picker-button');
+                    let index = styleAttribute.indexOf('#') + 1;
+
+                    colorPicker.value = styleAttribute.slice(index);
+                    colorPickerButton.style.backgroundColor = `#${colorPicker.value}`;
+                    colorPickerButton.style.color = '#fff';
+                }
+            } else if (e.button === 2) {
+                div.removeAttribute('style');
+                opacity = 1;
+            }
+        });
+    });
+}
+
 // Slider event
 const slider = document.querySelector('.slider');
 slider.addEventListener('change', () => {
@@ -46,6 +119,9 @@ let containerSize = Math.pow(containerBoxes, 2);
 
 // Create grid divs
 generateGrid(container);
+
+// Apply mouseover events for each div
+applyEventListenersToGrid();
 
 // Detect mouse down/up/leave. Used for the conditional in mouseover event below
 let mouseDown = 0;
@@ -84,65 +160,6 @@ document.body.addEventListener('mouseleave', e => {
 
 // Prevents right click menu inside container
 container.addEventListener('contextmenu', e => e.preventDefault());
-
-// Apply mouseover events for each div
-function applyEventListenersToGrid() {
-    const allDivs = document.querySelectorAll('.grid-item');
-    allDivs.forEach(div => {
-        div.addEventListener('mouseover', e => {
-            if (mouseDown) {
-                if (rainbowOn === 1) {
-                    // Generate 3 random numbers to create an RGB color, converts to hex & applies style to div
-                    let rgb = [];
-                    for (let i = 1; i <= 3; i++) {
-                        rgb.push(randomNum(0, 256));
-                    }
-
-                    const hexValue = rgbToHex(rgb[0], rgb[1], rgb[2]);
-
-                    div.setAttribute('style', `background-color: ${hexValue}`);
-                } else {
-                    div.setAttribute('style', `background-color: #${colorPicker.value}`);
-                }
-            } else if (rightMouseDown) {
-                div.removeAttribute('style');
-            }
-        });
-
-        div.addEventListener('mousedown', e => {
-            if (e.button === 0) {
-                if (rainbowOn === 1) {
-                    // Generate 3 random numbers to create an RGB color, converts to hex & applies style to div
-                    let rgb = [];
-                    for (let i = 1; i <= 3; i++) {
-                        rgb.push(randomNum(0, 256));
-                    }
-
-                    const hexValue = rgbToHex(rgb[0], rgb[1], rgb[2]);
-
-                    div.setAttribute('style', `background-color: ${hexValue}`);
-                } else {
-                    div.setAttribute('style', `background-color: #${colorPicker.value}`);
-                }
-            } else if (e.button === 1) {
-                const styleAttribute = div.getAttribute('style');
-
-                if (styleAttribute !== null) {
-                    const colorPickerButton = document.querySelector('#color-picker-button');
-                    let index = styleAttribute.indexOf('#') + 1;
-
-                    colorPicker.value = styleAttribute.slice(index);
-                    colorPickerButton.style.backgroundColor = `#${colorPicker.value}`;
-                    colorPickerButton.style.color = '#fff';
-                }
-            } else if (e.button === 2) {
-                div.removeAttribute('style');
-            }
-        });
-    });
-}
-
-applyEventListenersToGrid();
 
 // Options
 
@@ -188,21 +205,40 @@ removeBorder.addEventListener('click', () => {
 });
 
 // Rainbow
-let rainbowOn = 0;
+let rainbowOn = false;
 
 const rainbowButton = document.querySelector('#rainbow');
 rainbowButton.addEventListener('click', () => {
-    if (rainbowOn === 0) {
+    if (!rainbowOn) {
         rainbowButton.classList.add('rainbow-gradient');
         rainbowButton.textContent = 'Rainbow: On';
-    } else if ((rainbowOn = 1)) {
+    } else if (rainbowOn) {
         rainbowButton.classList.remove('rainbow-gradient');
         rainbowButton.textContent = 'Rainbow: Off';
     }
 
-    if (rainbowOn === 0) {
-        rainbowOn = 1;
-    } else if (rainbowOn === 1) {
-        rainbowOn = 0;
+    if (!rainbowOn) {
+        rainbowOn = true;
+    } else if (rainbowOn) {
+        rainbowOn = false;
+    }
+});
+
+// Realistic Mode - Sets darkening on each mouseover
+let realisticMode = false;
+
+const realisticElement = document.querySelector('#realistic-mode');
+
+realisticElement.addEventListener('click', () => {
+    if (realisticMode === false) {
+        realisticElement.textContent = 'Realistic: On';
+    } else if (realisticMode === true) {
+        realisticElement.textContent = 'Realistic: Off';
+    }
+
+    if (!realisticMode) {
+        realisticMode = true;
+    } else if (realisticMode) {
+        realisticMode = false;
     }
 });
